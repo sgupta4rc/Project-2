ffed0a82a2f01a4b261df298d5d9f8d55e0aca0f
Suhaani Gupta <sgupta@student.rccd.edu>
2022-07-31 16:03:41 -0400

    added gamebegin and gameintro functions. moved code from main to these functions.

--- blackjack/blackjack_P2.cpp
+++ blackjack/blackjack_P2.cpp
@@ -8,7 +8,8 @@
 
 using namespace std;
 
-void gameintro(vector<string>& log, int balance[][], string& playername) {
+void gameintro(vector<string>& log, int& balance, string& playername) {
+    string str;
 
     cout << "File to write log could not be open. So, no log of the session will be written" << endl;
     cout << "This is Suhaani's blackjack game. You play against the dealer" << endl;
@@ -17,12 +18,88 @@ void gameintro(vector<string>& log, int balance[][], string& playername) {
     getline(cin, playername);
     cout << "Welcome " << playername << " to Suhaani's Blackjack game" << endl;
     cout << "How many chips do you want to buy? (Each is worth one dollar)" << endl << endl;
-    cin >> balance[0][0];
-    cout << playername << " is playing Suhaani's blackjack with " << balance << " dollars" << endl;
+    cin >> balance;
+    cout << playername << " is playing Suhaani's blackjack with " << to_string(balance) << " dollars" << endl;
     cout << "-------------------------------------------------------------------" << endl << endl;
-
+    str = playername + " is playing with " + to_string(balance) + " Dollars";
     cout << "new Session for play started by started by " << playername << endl;
-    log.push_back(playername + " is playing with " + balance + " Dollars");//pushing log to log vector
+    log.push_back(str);//pushing log to log vector
+}
+
+void gamebegin(vector <string>& log, int& playersum, int& dealersum, string& playercards,string& dealercards) {
+
+    int firstcard;
+    int secondcard;
+    int dealercard;
+    string str;
+    string cards;
+   
+
+    srand(time(NULL));
+    cout << "Dealer serving your first two cards. Here it goes... " << endl << endl;
+    str = "Dealer serving your first two cards. Here it goes... \n \n";
+    log.push_back(str);
+    
+    firstcard = 1 + (rand() % 13);//generating different values using rand ranging from 1 to 13
+    if (firstcard >= 11 && firstcard <= 13) //it is face card. Assume it 10
+        firstcard = 10;
+
+    secondcard = 1 + (rand() % 13);//generating different values using rand ranging from 1 to 13
+    if (secondcard >= 11 && secondcard <= 13) //second card is face card. Assume it 10
+        secondcard = 10;
+
+    if (firstcard == 1) { //if ace card
+        firstcard = 11; //assume 11
+        cout << "your first card is 1. Hence it is treated as 11" << endl;
+        // bjfile<<"your first card is 1. hence it is treated as 11"<<endl;//writing log to file
+    }
+
+    if (firstcard != 11 && secondcard == 1) { //if first not Ace, then assume second 11
+        cout << "Your second card is 1. Hence it is treated as 11" << endl;
+        log.push_back("Your second card is 1. Hence it is treated as 11 \n");   
+        secondcard = 11;
+    }
+
+    playersum += firstcard;
+    playersum += secondcard;
+    cards = to_string(firstcard) + ", " + to_string(secondcard);
+
+    cout << "Your cards : " << cards << endl << "Total= " << playersum << endl << endl;
+    str= "Your cards : " + cards + "\n Total= " + to_string(playersum) + "\n \n";
+    log.push_back(str);
+
+    if (playersum == 21) {//it is blackjack for player 
+        cout << "Wow! you hit a blackjack.." << endl;
+        log.push_back("Wow! you hit a blackjack..\n");
+    }
+
+    //Now dealer will open his first card
+    //.....................................................................
+    cout << "Dealer's Turn..Dealer open her first card..." << endl;
+    log.push_back("Dealer's Turn..Dealer open her first card...\n");
+
+    dealercard = 1 + (rand() % 13);//generating different values using rand ranging from 1 to 13
+
+    if (dealercard >= 11 && firstcard <= 13) {//dealer got face card,make it 10
+        dealercard = 10;
+    }
+    if (dealercard == 1) {
+        dealercard = 11;
+        cout << "Dealer first card is 1, hence it is treated as 11" << endl << endl;
+        log.push_back("Dealer first card is 1, hence it is treated as 11\n");
+    }
+    cout << dealercard << endl;
+    str = dealercards + "\n";
+    log.push_back(str);
+    dealersum += dealercard;
+    dealercards = to_string(dealercard);
+
+    cout << "Dealer Total is : " << dealersum << endl << endl;
+    str = "Dealer Total is : " + dealersum + '\n \n';
+
+    //................................................................................
+    cout << "------------------------------------------------------" << endl;
+    cout << "Your turn now.....";
 }
 
 
@@ -35,89 +112,29 @@ int main() {
     bool gamemasterloop = true;
     bool dealerBust = false;
     char response;
-    int firstcard;
-    int secondcard;
-    int dealercard;
+    
     int newcard;
-    int sum = 0;
+    int playersum = 0;
     int dealersum = 0;
     int BET = 1;
-    int balance;
-    balance = 100;
-    string cards;
+    int dealercard;
+ 
+    string playercards;
     string dealercards;
     string playername;
     ofstream bjfile;
+
     int const ROWMAX = 100; //assume player will play maximum 100 sessions in the play
 
     vector <string> log;
-    int balance[ROWMAX][2] = {}; //array to save current balance points. 
-
+    int balance[ROWMAX][2] = { {} }; //array to save current balance points. 
+    int session = 1;
     //open a txt file to write the session log as it is important to write log in casino
 
-
+    gameintro(log, balance[session][0], playername);
+    gamebegin(log, playersum, dealersum, playercards, dealercards);
+    
     while (gamemasterloop) {
-
-        srand(time(NULL));
-        cout << "Dealer serving your first two cards. Here it goes... " << endl << endl;
-        bjfile << "Dealer serving your first two cards. Here it goes... " << endl << endl; //writing to log file
-        firstcard = 1 + (rand() % 13);//generating different values using rand ranging from 1 to 13
-        if (firstcard >= 11 && firstcard <= 13) //it is face card. Assume it 10
-            firstcard = 10;
-
-        secondcard = 1 + (rand() % 13);//generating different values using rand ranging from 1 to 13
-        if (secondcard >= 11 && secondcard <= 13) //second card is face card. Assume it 10
-            secondcard = 10;
-
-        if (firstcard == 1) { //if ace card
-            firstcard = 11; //assume 11
-            cout << "your first card is 1. Hence it is treated as 11" << endl;
-            // bjfile<<"your first card is 1. hence it is treated as 11"<<endl;//writing log to file
-        }
-
-        if (firstcard != 11 && secondcard == 1) { //if first not Ace, then assume second 11
-            cout << "Your second card is 1. Hence it is treated as 11" << endl;
-            //bjfile<<"Your second card is 1. Hence it is treated as 11"<<endl;//writing log to file
-            secondcard = 11;
-        }
-        sum += firstcard;
-        sum += secondcard;
-        cards = to_string(firstcard) + ", " + to_string(secondcard);
-
-        cout << "Your cards : " << cards << endl << "Total= " << sum << endl << endl;
-        //bjfile<<"Your cards : "<<cards<<endl<<"Total= "<<sum<<endl<<endl; //writing to log file
-
-        if (sum == 21) {//it is blackjack for player 
-            cout << "Wow! you hit a blackjack.." << endl;
-            //bjfile<<"Wow! you hit a blackjack.."<<endl;//writing log to file
-        }
-        //Now dealer will open his first card
-        //.....................................................................
-        cout << "Dealer's Turn..Dealer open her first card..." << endl;
-        //bjfile<<"Dealer's Turn..Dealer open her first card..."<<endl;//writing log to file 
-
-        dealercard = 1 + (rand() % 13);//generating different values using rand ranging from 1 to 13
-
-        if (dealercard >= 11 && firstcard <= 13) {//dealer got face card,make it 10
-            dealercard = 10;
-        }
-        if (dealercard == 1) {
-            dealercard = 11;
-            cout << "Dealer first card is 1, hence it is treated as 11" << endl << endl;
-            // bjfile<<"Dealer first card is 1, hence it is treated as 11"<<endl<<endl;  //writing log to file   
-        }
-        cout << dealercard << endl;
-        //bjfile<<dealercard<<endl;//writing log to file
-        dealersum += dealercard;
-        dealercards = to_string(dealercard);
-
-        cout << "Dealer Total is : " << dealersum << endl << endl;
-        //bjfile<<"Dealer Total is : "<<dealersum<<endl<<endl;//writing log to file
-
-        //................................................................................
-        cout << "------------------------------------------------------" << endl;
-        cout << "Your turn now.....";
-
         while (gameinnerloop) { //execute player has no blackjack. loop for player to takeout more cards untill stand, burst or blackjack
             cout << "do you want stand or hit? (h for hit / s for stand) " << endl << endl;
             // bjfile<<"do you want stand or hit? (h for hit / s for stand) "<<endl<<endl;//writing log to file
@@ -128,23 +145,24 @@ int main() {
                 if (newcard >= 11 && newcard <= 13)//face card. So make it 10
                     newcard = 10;
 
-                if (newcard == 1 && (sum + 11) <= 21)//assume new card 11 if sum is less than 21, otherwise be it 1
+                if (newcard == 1 && (dealersum + 11) <= 21)//assume new card 11 if sum is less than 21, otherwise be it 1
                     newcard = 11;
-                sum += newcard;
-                cards = cards + ", " + to_string(newcard);
-                cout << "Your cards : " << cards << " total = " << sum << endl;
+                playersum += newcard;
+                playercards = playercards + ", " + to_string(newcard);
+                cout << "Your cards : " << playercards << " total = " << playersum << endl;
                 // bjfile<<"Your cards : "<<cards<<" total = "<<sum<<endl;//writing log to file
 
-                if (sum == 21) { //blackjack for player
+                if (playersum == 21) { //blackjack for player
                     cout << "Wow! you hit a blackjack.." << endl;
                     //bjfile<<"Wow! you hit a blackjack.."<<endl;//writing log to file
                     gameinnerloop = false;
                 }
 
-                if (sum > 21) { //player is busted and dealer wins
+                if (playersum > 21) { //player is busted and dealer wins
                     cout << "You are bursted. You lose $" << BET << " to the dealer" << endl << endl;
                     //bjfile<<"You are bursted. You lose $"<<BET<<" to the dealer"<<endl<<endl;//writing log to file
-                    balance = balance - BET;
+                    balance[session][0]-= BET;
+                    balance[session][1] += BET;
                     gameinnerloop = false;
                     playerBust = true;
                 }
@@ -166,7 +184,7 @@ int main() {
             do {
                 dealercard = 1 + (rand() % 13);//generating different values using rand ranging from 0to13
 
-                if (dealercard >= 11 && firstcard <= 13) //dealer got face card. make it 10
+                if (dealercard >= 11 && dealercard <= 13) //dealer got face card. make it 10
                     dealercard = 10;
 
                 if (dealercard == 1 && ((dealersum + 11) <= 21)) //dealer will treat 1 as 11 only when is less than 21
@@ -184,7 +202,7 @@ int main() {
                     //cout << "Dealer paid you $" << BET << endl;
                     //  bjfile<<"Dealer bursted out... Hence you won the game"<<endl;//writing log to file
                     //  bjfile<<"Dealer paid you $"<<BET<<endl;//writing log to file
-                    //balance += BET;
+                    //balance[session] += BET;
                     //cout << "Your new balance is : $" << balance << endl;
                     //   bjfile<<"Your new balance is : $"<<balance<<endl;//writing log to file
                     dealerflag = false;
@@ -197,7 +215,7 @@ int main() {
                     dealerflag = false;
                 }
 
-                else if (dealersum == 21 && sum != 21) {
+                else if (dealersum == 21 && playersum != 21) {
                     cout << "Dealer got blackjack!" << endl;
                     // bjfile<<"Dealer got blackjack!"<<endl;//writing log to file
                     //balance = balance - BET;
@@ -214,29 +232,34 @@ int main() {
 
             if (dealerBust) {
                 cout << "dealer busted out, you WON!" << endl;
-                balance += BET;
+                balance[session][0] += BET;
+                balance[session][1] -= BET;
+
                 cout << "Dealer lost her " << BET << " dollar bet to you" << endl;
-                cout << "Your new balance is : $" << balance << endl;
+                cout << "Your new balance is : $" << balance[session][0] << endl;
             }
 
-            else if (dealersum > sum) {
+            else if (dealersum > playersum) {
                 cout << "Dealer Won!. Your " << BET << " $ bet went to the dealer" << endl;
                 // bjfile<<"Dealer Won!. Your "<<BET<<" $ bet went to the dealer"<<endl;//writing log to file
-                balance -= BET;
-                cout << "Your new balance is : $" << balance << endl;
+                balance[session][0] -= BET;
+                balance[session][1] += BET;
+                cout << "Your new balance is : $" << balance[session][0] << endl;
                 // bjfile<<"Your new balance is : $"<<balance<<endl;//writing log to file
             }
-            else if (sum > dealersum) {
+            else if (playersum > dealersum) {
                 cout << "You Won" << endl;
                 cout << "Dealer paid you $" << BET << endl;
                 //  bjfile<<"You Won"<<endl;//writing log to file
                 //  bjfile<<"Dealer paid you $"<<BET<<endl;//writing log to file
-                balance += BET;
-                cout << "Your new balance is : $" << balance << endl;
+                balance[session][0] += BET;
+                balance[session][1] -= BET;
+
+                cout << "Your new balance is : $" << balance[session][0] << endl;
                 // bjfile<<"Your new balance is : $"<<balance<<endl;//writing log to file
 
             }
-            else if (sum == 21 && dealersum == 21)
+            else if (playersum == 21 && dealersum == 21)
             {
                 cout << "Both dealer and you got blackjacks. Hence it is draw. Your bet is returned" << endl;
             }
@@ -255,19 +278,20 @@ int main() {
 
         if (response == 'n' || response == 'n') {
             cout << "Thanks for playing Suhaani's blackjack" << endl;
-            cout << "Your current balance is " << balance << " dollars" << endl;
+            cout << "Your current balance is " << balance[session][0] << " dollars" << endl;
             cout << "Return counters and take your money back" << endl;
             cout << "--------------------------------------------";
             gamemasterloop = false;
         }//end of if
         else {
-            sum = 0;
+            playersum = 0;
             dealersum = 0;
             dealerflag = true;
             gameloop = true;
             gameinnerloop = true;
             playerBust = false;
             dealerBust = false;
+            session++;
         }
 
     }// end of mastergameloop
